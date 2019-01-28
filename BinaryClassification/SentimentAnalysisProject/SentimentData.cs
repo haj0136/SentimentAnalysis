b31083c6444751fa908f48fa1b61a61c5847665c
maras49 <hajdk@seznam.cz>
2019-01-28 18:46:06 +0100

    add 1.  project

---
+++ BinaryClassification/SentimentAnalysisProject/SentimentData.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.ML.Data;
+
+namespace SentimentAnalysisProject
+{
+    public class SentimentData
+    {
+        [Column(ordinal: "0", name: "Label")] public float Sentiment;
+        [Column(ordinal: "1")] public string SentimentText;
+        public float Probability;
+    }
+}
